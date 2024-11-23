62e228a6276e11f021ab772d7c84e333c75b4327
Sheena Verana <67240854+sheenamrv@users.noreply.github.com>
2024-11-22 19:11:58 -0500

    Update basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = (process.env.NODE_ENV = "production");
 const nextConfig = {
+  basePath: isProd ? "/website-2024" : "",
   output: "export",
   distDir: "dist",
 };
